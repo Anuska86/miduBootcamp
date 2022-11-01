06e585d06a004507e1c016bc10081591026ec898
Anuska86 <anasrm86@gmail.com>
2022-11-01 16:57:47 +0100

    class4_Bootcamp

---
+++ class4/src/App.js
@@ -0,0 +1,29 @@
+
+import "./styles.css";
+
+const notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2020-11-01T 13:17:30.098Z",
+    important: true,
+  },
+
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    date: "2020-11-01T 14:17:30.098Z",
+    important: false,
+  },
+
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTML",
+    date: "2020-11-01T 15:17:30.098Z",
+    important: true,
+  },
+];
+
+export default function App() {
+  return <h1>Bootcamp class 4</h1>;
+}
--- my-counter/src/index.js
+++ my-counter/src/index.js
@@ -2,6 +2,14 @@ import { useState } from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 
+const WarningNotUsed = () => {
+  return <h3>The counter has not been used yet</h3>;
+};
+
+const ListofClicks = ({ clicks }) => {
+  return <p>{clicks.join(", ")}</p>;
+};
+
 const App = () => {
   //const [left, setLeft] = useState(0);
   // const [right, setRight] = useState(0);
@@ -9,25 +17,26 @@ const App = () => {
   const [counters, setCounters] = useState({
     left: 0,
     right: 0,
-    clicks: 0,
-    message: "Hello world",
   });
+
+  const [clicks, setClicks] = useState([]);
 
   const handleClickLeft = () => {
     const newCountersState = {
       ...counters,
       left: counters.left + 1,
-      clicks: counters.clicks + 1,
     };
     setCounters(newCountersState);
+    setClicks((prevClicks) => [...prevClicks, "L"]);
   };
 
   const handleClickRight = () => {
     setCounters({
       ...counters,
       right: counters.right + 1,
-      clicks: counters.clicks + 1,
     });
+
+    setClicks((prevClicks) => [...prevClicks, "R"]);
   };
 
   return (
@@ -36,8 +45,12 @@ const App = () => {
       <button onClick={handleClickLeft}>left</button>
       <button onClick={handleClickRight}>right</button>
       {counters.right}
-      <p>Clicks totales: {counters.clicks}</p>
-      <p>{counters.message}</p>
+      <p>Clicks totales: {clicks.length}</p>
+      {clicks.length === 0 ? (
+        <WarningNotUsed />
+      ) : (
+        <ListofClicks clicks={clicks} />
+      )}
     </div>
   );
 };
